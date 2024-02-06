cc432ccec9ae73f689141c05848f4d4e070a0df3
Chaimaa El Ghandori <elghandori.chaimaa@gmail.com>
2024-02-06 10:11:06 -0800

    Done

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Module creates a class called Amenity"""
 from models.base_model import BaseModel
+
+
 class Amenity(BaseModel):
- """This class manages amenity objects
- Represents an amenity.
- Attributes:
- name (str): Indicates the name of the amenity.
- """
- name = ""
+    """This class manages amenity objects
+    Represents an amenity.
+    Attributes:
+        name (str): Indicates the name of the amenity.
+    """
+    name = ""
--- models/city.py
+++ models/city.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Module creates a class called City"""
 from models.base_model import BaseModel
+
+
 class City(BaseModel):
- """This class manages city objects and
- Represents a city.
- Attributes:
- state_id (str): Indicates the state id.
- name (str): Indicates the name of the city.
- """
- state_id = ""
- name = ""
+    """This class manages city objects and
+    Represents a city.
+    Attributes:
+        state_id (str): Indicates the state id.
+        name (str): Indicates the name of the city.
+    """
+    state_id = ""
+    name = ""
