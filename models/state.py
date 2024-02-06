6cf409fe0446a231281da979a16f6484d3a58d29
Chaimaa El Ghandori <elghandori.chaimaa@gmail.com>
2024-02-06 10:20:39 -0800

    Done

--- models/state.py
+++ models/state.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Module creates a class called State and defines it"""
 from models.base_model import BaseModel
+
+
 class State(BaseModel):
- """Class manages state objects
- Represents a state.
- Attributes:
- name (str): Indicates the name of the state.
- """
- name = ""
+    """Class manages state objects
+    Represents a state.
+    Attributes:
+        name (str): Indicates the name of the state.
+    """
+    name = ""
